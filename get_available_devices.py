a80d13c963fe6faffe70eae791b276b8573c16c6
fxia22 <xf1280@gmail.com>
2018-08-13 22:40:40 -0700

    fix

--- get_available_devices.py
+++ get_available_devices.py
@@ -5,7 +5,7 @@ num_devices = int(subprocess.check_output(["./build/query_devices"]))
 available_devices = []
 for i in range(num_devices):
     try:
-        if "NVIDIA" in subprocess.check_output(["./build/test_device",  str(i)]):
+        if b"NVIDIA" in subprocess.check_output(["./build/test_device",  str(i)]):
             available_devices.append(i)
     except subprocess.CalledProcessError as e:
         print(e)
